6513b634dd9f39730e6caae5b99f5a5ee9050e37
kiloZ333 <leo_berraz0395@hotmail.com>
2021-12-16 11:19:12 -0300

    Desañadido el "/boletines"

--- BOLETINES/javascript/main.js
+++ BOLETINES/javascript/main.js
@@ -11,7 +11,7 @@ links.forEach(function(link){
         content.classList.add("animate__animated");
         
         setTimeout(function() {
-            location.href = "/boletines";
+            location.href = "/";
         },600); 
     });
 });
