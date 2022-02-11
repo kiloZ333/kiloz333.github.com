7a93e3dcea93c536b50b4fd777e7f3db21e9bd5a
kiloZ333 <leo_berraz0395@hotmail.com>
2022-02-11 19:03:00 -0300

    Sitio Web Personal

---
+++ Creación de PROYECTO FINAL - I/style.js
@@ -0,0 +1,11 @@
+const buttonMenu = document.querySelector(".icono-del-menu");
+let menuAbierto = false;
+buttonMenu.addEventListener("click",()=>{
+    if (!menuAbierto) {
+    document.getElementById("menu").style.marginTop = "44px";
+    menuAbierto = true;
+    } else {
+        document.getElementById("menu").style.marginTop = "-80%";
+        menuAbierto = false;
+    }
+})
